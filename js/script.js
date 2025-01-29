92acb0c844f68c631c51f09ce3bf34713c04a566
Joaquim <jocaq124@gmail.com>
2025-01-29 11:46:26 -0300

    Linkado o api do google

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    //Ocultar a 'div-erro'
+    $('#div-erro').hide();
+async function carregarDados() {
+}
+//Tornar o método visível para os eventos
+window.carregarDados = carregarDados;
+});
